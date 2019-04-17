36c44ffd73501c3b0ad6cb7de64a0f706cea66c6
lost222 <1610299@mail.nankai.edu.cn>
2019-04-17 12:30:06 +0800

    004 added

---
+++ 004/hw004.py
@@ -0,0 +1,11 @@
+import matplotlib
+matplotlib.use('TkAgg')
+import numpy as np
+from sklearn import neighbors
+from sklearn.model_selection import train_test_split
+from matplotlib import pyplot as plt
+from scipy.spatial import distance
+from scipy import stats
+
+
+
