4f86ef464104172e08f27db3c864fbfd569d2f5b
lost222 <1610299@mail.nankai.edu.cn>
2019-04-18 22:15:00 +0800

    python class problem

--- 004/hw004.py
+++ 004/hw004.py
@@ -26,20 +26,22 @@ def read_csc(path):
 def cal_m(X_mat):
     return np.mean(X_mat, axis=0)
 
+
 def cal_cov(x, y):
     m_x = np.mean(x)
     m_y = np.mean(y)
     return np.sum((x - m_x) * (y - m_y)) / len(x)
 
+
 def cal_gaussian_P(x, mu, sq_sigma):
     a = -(x - mu)**2 / (2*sq_sigma)
     return np.e**a / (np.sqrt(2*np.pi)*np.sqrt(sq_sigma))
 
-class Bayes:
-    mu_list = []
-    sq_sigma_list = []
 
+class Bayes:
     def __init__(self, X, Y):
+        self.mu_list = []
+        self.sq_sigma_list = []
         self.X = X
         self.Y = Y
         (row, col) = X.shape
@@ -51,7 +53,7 @@ class Bayes:
     def cal_p_Yey(self, y):
         """cal P(Y=y)"""
         a = np.sum(self.Y == y) / self.Y.shape[0]
-        return a
+        return np.log(a)
 
     def cal_p_Xex(self, x):
         """cal P(X=x) x is an array"""
@@ -59,7 +61,7 @@ class Bayes:
         for i in range(len(x)):
             P_list.append(cal_gaussian_P(x[i], self.mu_list[i], self.sq_sigma_list[i]))
         P_list = np.log(P_list)
-        return np.e**np.sum(P_list)
+        return np.sum(P_list)
 
 
 def cal_p_Xx_when_Yy(X, x, Y, y):
@@ -76,21 +78,20 @@ def cal_p_Yy_when_Xx(X, x, Y, y):
     P_Xx_when_Yy = cal_p_Xx_when_Yy(X, x, Y, y)
     ba = Bayes(X, Y)
     P_Yy = ba.cal_p_Yey(y)
-    P_Xx = ba.cal_p_Xex(x)
-    return P_Xx_when_Yy * P_Yy / P_Xx
+    # P_Xx = ba.cal_p_Xex(x)
+    return P_Xx_when_Yy + P_Yy
 
 
 
 def predict(X, x, Y):
     """predict y when X=y"""
     y = Y[0]
-    P_Yy_whenXx = 0
-    for c_k in set(Y):
-        P = cal_p_Yy_when_Xx(X, x, Y, c_k)
-        if P_Yy_whenXx < P:
-            y = c_k
-            P_Yy_whenXx = P
-    return y
+    P_Yy_whenXx = []
+    for c_k in range(3):
+        P = cal_p_Yy_when_Xx(X, x, Y, c_k + 1)
+        P_Yy_whenXx.append(P)
+    return np.argmax(P_Yy_whenXx) + 1
+
 
 
 
@@ -100,6 +101,7 @@ if __name__ == '__main__':
     X, Y = read_csc("wine.data")
     # 函数本身能保证分层采样
     X_train, X_test, Y_train, Y_test = train_test_split(X, Y, random_state=42)
+
     Y_predicted = []
     for x in X_test:
         y = predict(X_train, x, Y_train)
@@ -110,10 +112,11 @@ if __name__ == '__main__':
     w = 1
 
     e = Y_predicted - Y_test
-    print(e)
+    print(Y_predicted)
+    print(Y_test)
     err = np.sum(e != 0) * w / len(e)
     print(err)
-
+    #
 
 
 
