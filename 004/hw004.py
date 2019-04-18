c7d5f87b0ff51ab1f77e0d7b2e2801285abf47bc
lost222 <1610299@mail.nankai.edu.cn>
2019-04-19 00:27:44 +0800

    need to locate ROC

--- 004/hw004.py
+++ 004/hw004.py
@@ -1,4 +1,5 @@
 import matplotlib
+
 matplotlib.use('TkAgg')
 import numpy as np
 from sklearn import neighbors
@@ -34,8 +35,8 @@ def cal_cov(x, y):
 
 
 def cal_gaussian_P(x, mu, sq_sigma):
-    a = -(x - mu)**2 / (2*sq_sigma)
-    return np.e**a / (np.sqrt(2*np.pi)*np.sqrt(sq_sigma))
+    a = -(x - mu) ** 2 / (2 * sq_sigma)
+    return np.e ** a / (np.sqrt(2 * np.pi) * np.sqrt(sq_sigma))
 
 
 class Bayes:
@@ -65,12 +66,10 @@ class Bayes:
 
 
 def cal_p_Xx_when_Yy(X, x, Y, y):
-
     """cal P(X=x|Y=y) """
     y_index = np.where(Y == y, Y, 0).nonzero()
     n_m = Bayes(X[y_index], Y[y_index])
     return n_m.cal_p_Xex(x)
-
 
 
 def cal_p_Yy_when_Xx(X, x, Y, y):
@@ -80,7 +79,6 @@ def cal_p_Yy_when_Xx(X, x, Y, y):
     P_Yy = ba.cal_p_Yey(y)
     # P_Xx = ba.cal_p_Xex(x)
     return P_Xx_when_Yy + P_Yy
-
 
 
 def predict(X, x, Y):
@@ -93,8 +91,9 @@ def predict(X, x, Y):
     return np.argmax(P_Yy_whenXx) + 1
 
 
-
-
+def predict_threshold(X, x, Y, c, t):
+    P = cal_p_Yy_when_Xx(X, x, Y, c)
+    return t < P
 
 
 if __name__ == '__main__':
@@ -102,23 +101,85 @@ if __name__ == '__main__':
     # 函数本身能保证分层采样
     X_train, X_test, Y_train, Y_test = train_test_split(X, Y, random_state=42)
 
-    Y_predicted = []
-    for x in X_test:
-        y = predict(X_train, x, Y_train)
-        Y_predicted.append(y)
-    Y_predicted = np.array(Y_predicted)
-
-    # 代价敏感
-    w = 1
-
-    e = Y_predicted - Y_test
-    print(Y_predicted)
-    print(Y_test)
-    err = np.sum(e != 0) * w / len(e)
-    print(err)
+    # Y_predicted = []
+    # for x in X_test:
+    #     y = predict(X_train, x, Y_train)
+    #     Y_predicted.append(y)
+    # Y_predicted = np.array(Y_predicted)
     #
+    # # 代价敏感
+    # w = 1
+    #
+    # e = Y_predicted - Y_test
+    # print(Y_predicted)
+    # print(Y_test)
+    # err = np.sum(e != 0) * w / len(e)
+    # print(err)
+    # # cal Confusion_Matrix
+    #
+    # for to in [1, 2, 3]:
+    #     CM = np.zeros((2, 2))
+    #     for i in range(len(Y_test)):
+    #         T = Y_test[i]
+    #         P = Y_predicted[i]
+    #         if T == to:
+    #             if P == to:
+    #                 CM[0, 0] += 1
+    #             else:
+    #                 CM[0, 1] += 1
+    #         else:
+    #             if P == to:
+    #                 CM[1, 0] += 1
+    #             else:
+    #                 CM[1, 1] += 1
+    #     print("Confusion_Matrix of class " + str(to))
+    #     print(CM)
+
+    # ROC
+    cla = 3
+
+    threadH = []
+
+    for x in X_test:
+        p = cal_p_Yy_when_Xx(X_train, x, Y_train, cla)
+        threadH.append(p)
 
 
 
 
+    Y_test_u = np.where(Y_test == cla, True, False)
+    FPRs = []
+    TPRs = []
+    mi = int(np.min(threadH))
+    ma = int(np.max(threadH))
+    step = int((ma - mi) / 10)
+    for t in range(mi, ma):
+        th = t
+        Y_predicted = []
+        for x in X_test:
+            p = predict_threshold(X_train, x, Y_train, cla, th)
+            Y_predicted.append(p)
+        # CM
+        CM = np.zeros((2, 2))
+        for i in range(len(Y_test_u)):
+            T = Y_test_u[i]
+            P = Y_predicted[i]
+            if T:
+                if P:
+                    CM[0, 0] += 1
+                else:
+                    CM[0, 1] += 1
+            else:
+                if P:
+                    CM[1, 0] += 1
+                else:
+                    CM[1, 1] += 1
+        TPR = CM[0, 0] / (CM[0, 0] + CM[1, 1])
+        FPR = CM[1, 0] / (CM[0, 1] + CM[1, 0])
+        TPRs.append(TPR)
+        FPRs.append(FPR)
 
+    print(FPRs)
+    print(TPRs)
+    plt.plot(FPRs, TPRs, '^-')
+    plt.show()
