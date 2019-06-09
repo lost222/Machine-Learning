d2cf2701ad9673c05eaba805d35771f759918558
lost222 <1610299@mail.nankai.edu.cn>
2019-06-09 09:13:54 +0800

    alpha test

--- Final/final.py
+++ Final/final.py
@@ -24,26 +24,27 @@ Y = np.transpose(Y)
 X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=42)
 
 
-loss_vec = ['hinge', 'log', 'modified_huber', 'squared_hinge', 'perceptron', 'squared_loss', 'huber', 'epsilon_insensitive', 'squared_epsilon_insensitive']
+loss_vec = ['hinge', 'log', 'huber']
 penalty_vec = ['l1', 'l2', 'elasticnet']
-
+alpha_list = [1e-7, 1e-5, 1e-3, 1e-1]
 
 for loo in loss_vec:
     for pe in penalty_vec:
-        clf = OneVsRestClassifier(SGDClassifier(loss=loo, penalty=pe, max_iter=1000))
-        # clf  = OneVsRestClassifier(SVC(kernel='linear')
-        # 送进SGD
-        print("")
-        clf.fit(X_train, Y_train)
-        Y_predict = clf.predict(X_test)
-        pre1 = average_precision_score(Y_test, Y_predict, average='samples')
-        pre2 = precision_score(Y_test, Y_predict, average='samples')
-        # precision_vec.append(precision)
-        states_str = "loss : " + loo + "\npenalty : " + pe
-        log_str = "average_precision_score = " + str(pre1) + "\nprecision_score = " + str(pre2)
-        print(states_str)
-        print(log_str)
-        print("")
+        for alpha in alpha_list:
+            clf = OneVsRestClassifier(SGDClassifier(loss=loo, alpha=alpha, penalty=pe, max_iter=1000))
+            # clf  = OneVsRestClassifier(SVC(kernel='linear')
+            # 送进SGD
+            print("")
+            clf.fit(X_train, Y_train)
+            Y_predict = clf.predict(X_test)
+            pre1 = average_precision_score(Y_test, Y_predict, average='samples')
+            pre2 = precision_score(Y_test, Y_predict, average='samples')
+            # precision_vec.append(precision)
+            states_str = "loss : " + loo + "\npenalty : " + pe
+            log_str = "average_precision_score = " + str(pre1) + "\nprecision_score = " + str(pre2)
+            print(states_str)
+            print(log_str)
+            print("")
 
 
 
